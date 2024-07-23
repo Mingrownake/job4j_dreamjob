723dc042ea14cf8ddb2da77f108a4c40aaadad7f
Mingrownake <nikitosvozh@gmail.com>
2024-07-23 21:24:54 +0300

    5. Просмотр, редактирование и удаление вакансии [#504850 #458777]

--- src/main/java/ru/job4j/controller/VacancyController.java
+++ src/main/java/ru/job4j/controller/VacancyController.java
@@ -2,10 +2,7 @@ package ru.job4j.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.model.Vacancy;
 import ru.job4j.repository.MemoryVacancyRepository;
 import ru.job4j.repository.VacancyRepository;
@@ -29,6 +26,38 @@ public class VacancyController {
     @PostMapping("/create")
     public String postCreatePage(@ModelAttribute Vacancy vacancy) {
         vacancyRepository.save(vacancy);
+        return "redirect:/vacancies";
+    }
+
+    @GetMapping("/{id}")
+    public String getById(Model model, @PathVariable int id) {
+        var optionalVacancy = vacancyRepository.findById(id);
+        if (optionalVacancy.isEmpty()) {
+            model.addAttribute("message", "Пользователь с данным id не найден");
+            return "error/404";
+        }
+        model.addAttribute("vacancy", optionalVacancy.get());
+        return "vacancies/one";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Vacancy vacancy, Model model) {
+        var isUpdateVacancy = vacancyRepository.update(vacancy);
+        if (!isUpdateVacancy) {
+            model.addAttribute("message", "Пользователь с данным id не найден");
+            return "error/404";
+        }
+        return "redirect:/vacancies";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Model model, @PathVariable int id) {
+        var optionalVacancy = vacancyRepository.findById(id);
+        if (optionalVacancy.isEmpty()) {
+            model.addAttribute("message", "Пользователь с данным id не найден");
+            return "error/404";
+        }
+        vacancyRepository.deleteById(id);
         return "redirect:/vacancies";
     }
 }
