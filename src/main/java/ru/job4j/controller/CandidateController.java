1cf0fd20ed3e12b7166a514ff21cf81c2630e4d8
Mingrownake <nikitosvozh@gmail.com>
2024-07-24 06:17:28 +0300

    7. Создание, просмотр, редактирование и удаление кандидатов [#504858 #459151]

--- src/main/java/ru/job4j/controller/CandidateController.java
+++ src/main/java/ru/job4j/controller/CandidateController.java
@@ -2,9 +2,8 @@ package ru.job4j.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.model.Candidate;
 import ru.job4j.repository.CandidateRepository;
 import ru.job4j.repository.MemoryCandidateRepository;
 
@@ -22,6 +21,44 @@ public class CandidateController {
     @GetMapping("/create")
     public String getCreatePage() {
         return "candidates/create";
+    }
+
+    @PostMapping("/create")
+    public String postCreate(@ModelAttribute Candidate candidate) {
+        candidateRepository.save(candidate);
+        return "redirect:/candidates";
+    }
+
+    @GetMapping("{id}")
+    public String getId(@PathVariable int id, Model model) {
+        var optionalCandidate = candidateRepository.findById(id);
+        if (optionalCandidate.isEmpty()) {
+            model.addAttribute("message", "Сотрудник с данным id не найден");
+            return "error/404";
+        }
+        model.addAttribute("candidate", optionalCandidate.get());
+        return "candidates/one";
+    }
+
+    @PostMapping("/update")
+    public String postUpdate(@ModelAttribute Candidate candidate, Model model) {
+        var isUpdate = candidateRepository.update(candidate);
+        if (!isUpdate) {
+            model.addAttribute("message", "Сотрудник с данным id не найден");
+            return "error/404";
+        }
+        return "redirect:/candidates";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String getDelete(Model model, @PathVariable int id) {
+        var optionalCandidate = candidateRepository.findById(id);
+        if (optionalCandidate.isEmpty()) {
+            model.addAttribute("message", "Сотрудник с данным id не найден");
+            return "error/404";
+        }
+        candidateRepository.deleteById(id);
+        return "redirect:/candidates";
     }
 
 }
